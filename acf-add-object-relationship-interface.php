9166ca65a8fb9b2fe3f9b72f9056d5bf1f603047
Jan Beck <delite.jcb@googlemail.com>
2015-08-18 13:53:53 +0200

    initial commit

---
+++ acf-add-object-relationship-interface.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: ACF - Add posts through relationship interface
+Description: Create posts from the relationship interface of the advanced custom fields plugin if they do not exist yet.
+Plugin URI: https://github.com/jancbeck/acf-add-object-relationship-interface
+Author: Jan Beck
+Author URI: http://jancbeck.com
+Version: 1.0
+License: GPL2
+Text Domain: acf-add-rel
+*/
+
+/*
+
+    Copyright (C) 2015 Jan Beck mail@jancbeck.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
